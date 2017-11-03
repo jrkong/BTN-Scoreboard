25016bf4e4dace79a158eda53c375c5500f08ca3
rlam17 <rlam17@users.noreply.github.com>
2017-11-03 11:06:51 -0400

    Now uses MySql connection
    TODO: Get it to connect to remote Db

--- Unity Scoreboard/Scoreboard Template/Scoreboard Template/DataHandler.cs
+++ Unity Scoreboard/Scoreboard Template/Scoreboard Template/DataHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +8,16 @@ using System.Data;
 using System.Windows;
 using System.IO;
 
+using MySql.Data.MySqlClient; //Jay's database is actually MySql, be sure to get this package
+
 namespace Scoreboard_Template
 {
     class DataHandler
     {
-        SqlConnection conn;
-        SqlCommand cmd;
+        //SqlConnection conn;
+        //SqlCommand cmd;
+        MySqlConnection conn;
+        MySqlCommand cmd;
         string query;
 
         public DataHandler()
@@ -28,12 +32,20 @@ namespace Scoreboard_Template
             bool exists = false;
             int databaseId = 0;
             query = "SELECT database_id FROM sys.databases WHERE Name = 'nightmares'";
-            conn = new SqlConnection("server=(local);Trusted_Connection=yes");
+            //conn = new SqlConnection("server=(local);Trusted_Connection=yes");  //Local db
+            MySqlConnectionStringBuilder connectionString = new MySqlConnectionStringBuilder();
+            connectionString.Add("Server", "99.227.52.70");
+            connectionString.Add("Port", "8080");
+            connectionString.Add("Database", "InjectionDataBase");
+            connectionString.Add("Username", "Pat");
+            connectionString.Add("Password", "GG");
+            
+            conn = new MySqlConnection(connectionString.ConnectionString);            
             try
             {
                 using (conn)
                 {
-                    using (cmd = new SqlCommand(query, conn))
+                    using (cmd = new MySqlCommand(query, conn))
                     {
                         conn.Open();
                         object result = cmd.ExecuteScalar();
@@ -57,8 +69,8 @@ namespace Scoreboard_Template
         public void initializeDatabase()
         {
             query = "CREATE DATABASE nightmares";
-            conn = new SqlConnection("server=(local);Trusted_Connection=yes");
-            cmd = new SqlCommand(query, conn);
+            conn = new MySqlConnection("server=(local);Trusted_Connection=yes");
+            cmd = new MySqlCommand(query, conn);
             try
             {
                 conn.Open();
@@ -70,8 +82,8 @@ namespace Scoreboard_Template
                 MessageBox.Show(ex.ToString());
             }
             query = File.ReadAllText(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\create_tables.sql")));
-            conn = new SqlConnection("server=(local);Trusted_Connection=yes;Initial Catalog=nightmares");
-            cmd = new SqlCommand(query, conn);
+            conn = new MySqlConnection("server=(local);Trusted_Connection=yes;Initial Catalog=nightmares");
+            cmd = new MySqlCommand(query, conn);
             try
             {
                 conn.Open();
@@ -94,12 +106,12 @@ namespace Scoreboard_Template
         public DataSet getScores()
         {
             DataSet ds = new DataSet();
-            conn = new SqlConnection("server=(local);Trusted_Connection=yes;Initial Catalog=nightmares");
+            conn = new MySqlConnection("server=(local);Trusted_Connection=yes;Initial Catalog=nightmares");
             query =
                 "SELECT TOP 40 l.score, u.username, l.sDate FROM dbo.leaderboard l " +
                 "INNER JOIN dbo.users u ON l.userId = u.id " +
                 "ORDER BY l.score DESC";
-            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
             try
             {
                 conn.Open();
